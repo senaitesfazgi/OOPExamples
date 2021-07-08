5859c2546e98d07caea7e4b8aa14afa3d1f0b832
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-07-08 15:05:52 -0600

    Added getter and setter to name property

---
+++ OOPExamples/Computer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExamples
+{
+    class Computer
+    {
+        protected string _name = "UNKNOWN";
+
+        public string name { 
+            get {
+                return _name;
+            } 
+
+            set {
+                _name = value;
+            }
+        }
+    }
+}
--- OOPExamples/Program.cs
+++ OOPExamples/Program.cs
@@ -7,6 +7,10 @@ namespace OOPExamples
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var comp = new Computer();
+            comp.name = "Generic Computer";
+            Console.WriteLine(comp.name);
+            Console.ReadLine();
         }
     }
 }
